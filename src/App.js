c03bb50c857b951dec6c7781cda04e9a86668ba2
vivek6325 <vivekairpalli@gmail.com>
2025-12-15 07:51:28 +0000

    Fix issues

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-// import AppStore from './components/AppStore'
+import AppStore from './components/AppStore'
 
-// import './App.css'
+import './App.css'
 
-// const App = () => <AppStore />
+const App = () => <AppStore />
 
-// export default App
-
-import { HashRouter, Routes, Route } from 'react-router-dom'
-
-function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </HashRouter>
-  )
-}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import App from './App'
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root'),
-// )
-
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom'
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <HashRouter>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
 )
